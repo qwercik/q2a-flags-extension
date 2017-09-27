9ca0ae0511d8d0e0ea243f96c0f3e87eea44c760
Eryk Andrzejewski <erykandrzejewski2000@gmail.com>
2017-09-27 20:37:48 +0200

    Add appending text inputes while clicking flag button

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 window.addEventListener('load', function() {
     var flagButtons = document.getElementsByClassName('qa-form-light-button-flag');
     for (var item of flagButtons) {
-        item.addEventListener('click', function () {
-            alert('You have flagged a post!');
+        item.removeAttribute('onclick');
+        item.setAttribute('type', 'button');
+        var element = item;
+        item.addEventListener('click', function (element) {
+            var post = this.parentNode;
+            var flagReason = document.createElement('input');
+            flagReason.setAttribute('type', 'text');
+            flagReason.setAttribute('class', 'qa-form-tall-text');
+            flagReason.setAttribute('name', 'flag_details');
+
+            post.appendChild(flagReason);
         });
     }
 });
